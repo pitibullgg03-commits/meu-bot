5f74bbd34f193b800455e4d1284f4eda221922a6
pitibullgg <pitibullgg03@gmail.com>
2026-04-28 13:04:20 -0300

    commit

---
+++ index.js
@@ -0,0 +1,81 @@
+const {
+  Client,
+  GatewayIntentBits,
+  ActionRowBuilder,
+  ButtonBuilder,
+  ButtonStyle,
+  Events,
+  EmbedBuilder
+} = require('discord.js');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent
+  ]
+});
+
+const CARGO_VERIFICADO = '1498403428982853692';
+const CARGO_NAO_VERIFICADO = '1498702244734832650';
+const CARGO_EXTRA = '1364330556434944091'; // novo cargo
+
+client.once('ready', () => {
+  console.log('Bot PRO ligado!');
+});
+
+
+// 👤 Quando alguém entra
+client.on(Events.GuildMemberAdd, member => {
+  member.roles.add(CARGO_NAO_VERIFICADO);
+});
+
+
+// 📩 Painel de verificação BONITO
+client.on(Events.MessageCreate, async message => {
+  if (message.content === '!painel') {
+
+    const embed = new EmbedBuilder()
+      .setTitle('🔥 SISTEMA DE VERIFICAÇÃO 🔥')
+      .setDescription('🛡️ **Bem-vindo ao servidor!**\n\nPara liberar o acesso completo, clique no botão abaixo.\n\n⚠️ Apenas usuários verificados podem acessar os canais.')
+      .setColor('#ff0000') // vermelho
+      .setThumbnail('https://i.postimg.cc/D0KR4xV5/Chat-GPT-Image-28-de-abr-de-2026-10-38-43.png') // logo exemplo
+      .setImage('https://i.postimg.cc/8CYScdPd/Chat-GPT-Image-28-de-abr-de-2026-12-36-40.png') // banner exemplo
+      .setFooter({ text: '🇧🇷 𝐂𝐀𝐕𝐄𝐑𝐍𝐀 𝐃𝐎𝐒 𝐆𝐀𝐌𝐄𝐑𝐒 🇧🇷 • Segurança ativa 🔒' });
+
+    const button = new ButtonBuilder()
+      .setCustomId('verificar')
+      .setLabel('🚀 Verificar-se')
+      .setStyle(ButtonStyle.Danger); // vermelho
+
+    const row = new ActionRowBuilder().addComponents(button);
+
+    await message.channel.send({
+      embeds: [embed],
+      components: [row]
+    });
+  }
+});
+
+
+// 🔥 Clique no botão
+client.on(Events.InteractionCreate, async interaction => {
+  if (!interaction.isButton()) return;
+
+  if (interaction.customId === 'verificar') {
+
+    const member = interaction.member;
+
+    await member.roles.add(CARGO_VERIFICADO);
+    await member.roles.add(CARGO_EXTRA); // 👈 adiciona o segundo cargo
+    await member.roles.remove(CARGO_NAO_VERIFICADO);
+
+    await interaction.reply({
+      content: '🔥 Você foi verificado com sucesso!',
+      ephemeral: true
+    });
+  }
+});
+
+client.login('MTQ5ODY3MzYxOTk2MDg2MDgxNA.GgSF1L.uVuJgIFmR8jh41C1mS-YUbg7DW5uuHdaDSPoPQ');
