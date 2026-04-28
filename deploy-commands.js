86ec9d770870a154e482655426a7afbb7d8143dd
pitibullgg03-commitsgit <pitibullgg03@gmail.com>
2026-04-28 18:41:51 -0300

    agora vai

--- deploy-commands.js
+++ deploy-commands.js
@@ -5,6 +5,11 @@ const commands = [
   new SlashCommandBuilder()
     .setName('painel')
     .setDescription('Envia o painel de verificação')
+    .toJSON(),
+
+  new SlashCommandBuilder()
+    .setName('ticket')
+    .setDescription('Abre o painel de tickets')
     .toJSON()
 ];
 
@@ -12,7 +17,7 @@ const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
 (async () => {
   try {
-    console.log('🔄 Registrando comando /painel...');
+    console.log('🔄 Registrando comandos...');
 
     await rest.put(
       Routes.applicationGuildCommands(
@@ -22,7 +27,7 @@ const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
       { body: commands }
     );
 
-    console.log('✅ /painel registrado com sucesso!');
+    console.log('✅ Comandos registrados com sucesso!');
   } catch (error) {
     console.error(error);
   }
