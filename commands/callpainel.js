5be7eafc06006d6627a6526bdb73a68d265e0f1b
pitibullgg03-commitsgit <pitibullgg03@gmail.com>
2026-05-01 13:28:29 -0300

    ff

--- commands/callpainel.js
+++ commands/callpainel.js
@@ -19,7 +19,7 @@ module.exports = {
     const embed = new EmbedBuilder()
       .setTitle('🪨 SISTEMA DE PROGRESSÃO • CAVERNA DOS GAMERS')
       .setColor('#ff0000')
-      .setThumbnail('hhttps://i.postimg.cc/D0KR4xV5/Chat-GPT-Image-28-de-abr-de-2026-10-38-43.png')
+      .setThumbnail('https://i.postimg.cc/D0KR4xV5/Chat-GPT-Image-28-de-abr-de-2026-10-38-43.png')
       .setImage('https://i.postimg.cc/7Z41Db50/Chat-GPT-Image-1-de-mai-de-2026-13-17-57.png')
 
       .setDescription(
